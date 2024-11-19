ae55624919cbcba5d8ee2341a825e340f76a3ae4
Brandon864 <ojbrandon2345@mail.com>
2024-11-19 14:12:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+function addingEventListener() {
+   
+    const input = document.getElementById('button');
+  
+  
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+  
+    input.addEventListener('click', clickAlert);
+  }
+  
+  
+  addingEventListener();
+  
